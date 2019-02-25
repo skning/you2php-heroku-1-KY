e1867520cd7ee8b2691738fc203d87e19f01ce8b
yilia <5064072@qq.com>
2019-02-25 10:23:07 +0800

    Update header.php

--- web/header.php
+++ web/header.php
@@ -1,6 +1,5 @@
 <?php
 error_reporting( E_ALL&~E_NOTICE );
-require_once "lang.conf.php";
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,12 +7,12 @@ require_once "lang.conf.php";
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php echo $headtitle ?></title>
-<meta name="keywords" content="<?php echo $lang['HEADER_KEYWORDS']?>" />
-<meta name="description" content="<?php echo $lang['HEADER_DESCRIPTION']?>" />
+<meta name="keywords" content="Youtube,Youtube镜像,Youtube直通车,Youtube镜像网站,Youtube视频代理,Youtube在线视频" />
+<meta name="description" content="Youtube镜像" />
 
 <meta content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0" name="viewport" />
 <link rel="stylesheet" href="//cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css" >
-<link href="https://cdn.bootcss.com/Swiper/4.0.6/css/swiper.min.css" rel="stylesheet">
+<link href="https://cdn.bootcss.com/Swiper/4.0.6/css/swiper.min.css" rel="stylesheet"> 
 <script src="//cdn.bootcss.com/jquery/2.1.4/jquery.js"></script>
 <script src="//cdn.bootcss.com/jqueryui/1.11.2/jquery-ui.js"></script>
 <link href="//cdn.bootcss.com/video.js/5.20.4/alt/video-js-cdn.min.css" rel="stylesheet" />
@@ -27,27 +26,29 @@ require_once "lang.conf.php";
 <body>
 <nav class="navbar navbar-expand-md bg-primary navbar-dark">
 <div class="container">
-	<a class="navbar-brand" href="./"><i class="fa d-inline fa-youtube-play"></i><b>&nbsp;<?php echo TITLENAME ?></b></a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+	<a class="navbar-brand" href="./"><i class="fa d-inline fa-youtube-play"></i><b>&nbsp;<?php echo TITLENAME ?></b></a>
+	<?php require_once('select_default_gl.php');?>
+	<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 	<div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
 		<ul class="navbar-nav">
 		</ul>
 		<div id="custom-search-input">
 		     <form action="search.php" style="margin-bottom:0px">
 			<div class="input-group col-md-12">
-
-				<input type="text" name="q"  class="search-query form-control" id="youtube" placeholder="<?php echo $lang['HEADER_SEARCH']?>"><span class="input-group-btn"><button class="btn btn-danger" type="submit" style="z-index:4"><span class="fa d-inline fa-search"></span></button></span>
+			   
+				<input type="text" name="q"  class="search-query form-control" id="youtube" placeholder="搜索 Youtube"><span class="input-group-btn"><button class="btn btn-danger" type="submit" style="z-index:4"><span class="fa d-inline fa-search"></span></button></span>
 				</div>
 			</form>
 		<script >
 $("#youtube").autocomplete({
     source: function(request, response){
-
+        
         var query = request.term;
-
+        
         $.ajax({
-            url: "./ajax/autocomplete.php?q="+query,
+            url: "./ajax/autocomplete.php?q="+query,  
             dataType: 'jsonp',
-            success: function(data, textStatus, request) {
+            success: function(data, textStatus, request) { 
                response( $.map( data[1], function(item) {
                     return {
                         label: item[0],
@@ -57,7 +58,7 @@ $("#youtube").autocomplete({
             }
         });
     },
-
+    
 });
 </script>
 		</div>
