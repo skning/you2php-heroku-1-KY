67a687a620a7038ebfa8e45383a168ec941ed9bd
kongyeliyouwo <36878970+kongyeliyouwo@users.noreply.github.com>
2019-03-24 22:18:46 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
-$gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'HK';
+$gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyAGkWWI9K6MBU1zQxGiH0Ni-XBAXXm0R1I');
+define('APIKEY', 'AIzaSyDZnujV2PYHBEt9_wemKXGr_h1-gLhtA_g');
 define('GJ_CODE', $gl);
-define('SITE_NAME', '名称');
-define('TITLENAME', '名称');
-define('EN2DEKEY', '0090909i604wkkfkak');
-define('EMAIL', '你的邮箱');
+define('SITE_NAME', 'ytb');
+define('TITLENAME', 'ytb');
+define('EN2DEKEY', 'fGXe3R9OH7');
+define('EMAIL', '1070604045@qq.com');
 ?>
