9691ad7af369cbc4a260c33bd199c1163f0a4c08
skning <35919885+skning@users.noreply.github.com>
2019-09-25 23:45:24 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'JP';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyAOp9jhaYOQ');
+define('APIKEY', 'AIzaSyAOp9jhaYch302bulXz8k8BZTscCPK8GOQ');
 define('GJ_CODE', $gl);
 define('SITE_NAME', '随TB');
 define('TITLENAME', '随TB');
